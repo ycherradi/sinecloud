648dcf71c4a253bbd28cfd562f27c0c071437ca8
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-03-30 21:03:07 +0100

    front end redux initial set up and navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import LoginForm from "./components/auth/LoginForm";
-import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/UsersList";
-import User from "./components/User";
-import { authenticate } from "./services/auth";
+import { authenticate } from "./store/session";
+import Home from "./components/Home/index";
+import Profile from "./components/Profile/index";
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
@@ -18,7 +16,7 @@ function App() {
       if (!user.errors) {
         setAuthenticated(true);
       }
-      setLoaded(true);
+      // setLoaded(true);
     })();
   }, []);
 
@@ -27,29 +25,20 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
-      <Switch>
-        <Route path="/login" exact={true}>
-          <LoginForm
-            authenticated={authenticated}
-            setAuthenticated={setAuthenticated}
-          />
-        </Route>
-        <Route path="/sign-up" exact={true}>
-          <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
-        </Route>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
-      </Switch>
-    </BrowserRouter>
+    <>
+        <NavBar
+                    authenticated={authenticated}
+                    setAuthenticated={setAuthenticated}
+                />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/profile' >
+            <Profile />
+          </Route>
+        </Switch> 
+    </>
   );
 }
 
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store';
 
+const store = configureStore();
+
+function Root() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    );
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
